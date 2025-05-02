a5dfb58098996f6bbdf9d870cbd65eca4ed78fe7
Pear770 <dim_dimih52@mail.ru>
2025-05-02 11:57:28 +0300

    Описание изменений

--- PNGppSkript/Program.cs
+++ PNGppSkript/Program.cs
@@ -4,9 +4,7 @@ using PNGppSkript.Compilator;
 Console.WriteLine("Hello, World!");
 
 Constructor t = new Constructor();
-t.AddByte(22);
-t.AddVoid();
-t.AddString("скомпилировал Console.WriteLine(\"Hello,World!\");");
-t.AddVoid();
-t.AddInt(1);
-t.DebugGenerate("C:\\Users\\COMP\\Documents\\csProjects\\PNGppSkript\\PNGppSkript\\test.png");
+List<string> list = new List<string>();
+string FilePath = Console.ReadLine();
+
+list.AddRange(File.ReadAllLines(filepath));
