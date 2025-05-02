e05024e250b707686e9a14d7507ce95c24f85791
Pear770 <dim_dimih52@mail.ru>
2025-05-02 12:26:56 +0300

    asdf

--- PNGppSkript/Program.cs
+++ PNGppSkript/Program.cs
@@ -4,9 +4,4 @@ using PNGppSkript.Compilator;
 Console.WriteLine("Hello, World!123");
 
 Constructor t = new Constructor();
-t.AddByte(22);
-t.AddVoid();
-t.AddString("скомпилировал Console.WriteLine(\"Hello,World!\");");
-t.AddVoid();
-t.AddInt(1);
-t.DebugGenerate("C:\\Users\\COMP\\Documents\\csProjects\\PNGppSkript\\PNGppSkript\\test.png");
+Console.ReadLine();
