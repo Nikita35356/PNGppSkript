9b41284b1cd825cd9fbe11dc33206819bac5df43
Pear770 <dim_dimih52@mail.ru>
2025-05-03 17:18:47 +0300

    test

--- PNGppSkript/Program.cs
+++ PNGppSkript/Program.cs
@@ -1,14 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using PNGppSkript.Compilator;
 
-Console.WriteLine("Hello, World!1234567");
+Console.WriteLine("Hello, World!7");
 
 Constructor t = new Constructor();
-
 Console.WriteLine("sys");
-
-t.AddByte(2);
-t.DebugGenerate("C:\\Users\\COMP\\source\\repos\\Nikita35356\\PNGppSkript\\PNGppSkript\\ttt.png");
 
 
 Thread hidePrintThread = new Thread(IDE.Program.Main);
